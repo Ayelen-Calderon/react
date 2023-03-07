f9b0b07a8396f1f0bca9b2b7f10513e533cb7b96
Ayelén-Calderón <calderonayelen89@gmail.com>
2023-03-07 13:01:47 -0300

    first commit

---
+++ primer-proyecto-react/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <img src="https://i0.wp.com/eltallerdehector.com/wp-content/uploads/2022/06/dd088-mickey-mouse-png-sin-fondo.png?fit=700%2C700&ssl=1" alt="" />
+     <h1>hola mundo</h1>
+    </div>
+  )
+}
+
+export default App
